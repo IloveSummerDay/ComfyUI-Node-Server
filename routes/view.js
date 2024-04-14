897f14b086617b08ebf867096cac812f434be946
Zluo <1923855461@qq.com>
2024-04-14 19:24:08 +0800

    4.14 sed commit

--- routes/view.js
+++ routes/view.js
@@ -86,7 +86,8 @@ async function handleUpOSS(imgs_already, client_id) {
             clientId: client_id
         })
     })
-    console.log("OSS result: ", result.data);
+    // console.log("OSS result: ", result.data);
+
     if (result.data.statusCode !== 200) return { ...errInfo, ...result.data, message: '服务器请求OSS存储出错' }
     for (let i = 0; i < fileNames.length; i++) {
         ossMap[fileNames[i]] = result.data.data[i]
